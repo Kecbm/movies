ccfd3b34184cae052de798324dd5b81d268b0a6f
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-15 18:19:30 -0300

    :sparkles: movie model

---
+++ src/Movies.Application/Models/Movie.cs
@@ -0,0 +1,9 @@
+namespace Movies.Application.Models;
+
+public interface Movie
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+	public required int YearOfRelease { get; init; }
+    public required List<string> Genres { get; init; } = new();
+}
