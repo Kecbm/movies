3289d11f455f72d64857a41b72deb4f55f638f21
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-17 18:03:28 -0300

    :construction: POST movies

--- src/Movies.Api/Controllers/MoviesController.cs
+++ src/Movies.Api/Controllers/MoviesController.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Movies.Contracts.Requests;
+using Movies.Application.Repositories;
 
 namespace Movies.Api.Controllers;
 
 [ApiController]
+[Route("api")]
 public class MoviesController : ControllerBase
 {
-    privare readonly IMovieRepository _movieRepository;
+    private readonly IMovieRepository _movieRepository;
 
     public MoviesController(IMovieRepository movieRepository)
     {
         _movieRepository = movieRepository;
+    }
+
+    // Tornar os campos da request obrigatórios
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        return Ok(request);
     }
 }
