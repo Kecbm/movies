c125a15d11a4325527fc39505b15df2070923f8d
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-17 18:00:44 -0300

    :bug: remove required in movieResponse

--- src/Movies.Contracts/Responses/MovieResponse.cs
+++ src/Movies.Contracts/Responses/MovieResponse.cs
@@ -2,8 +2,8 @@ namespace Movies.Contracts.Requests;
 
 public class MovieResponse
 {
-    public required Guid Id { get; init; }
-    public required string Title { get; init; }
-	public required int YearOfRelease { get; init; }
-	public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+    public Guid Id { get; init; }
+    public string Title { get; init; }
+	public int YearOfRelease { get; init; }
+	public IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
 }
