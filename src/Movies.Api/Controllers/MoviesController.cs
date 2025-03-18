2c851514dd39cac8016d031060f1778be9f7a519
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-18 14:16:51 -0300

    :sparkles: GET movie

--- src/Movies.Api/Controllers/MoviesController.cs
+++ src/Movies.Api/Controllers/MoviesController.cs
@@ -25,4 +25,19 @@ public class MoviesController : ControllerBase
         // The path: Headers.Location
         return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
     }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> GetAsync(Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        var response = movie.MapToResponse();
+
+        return Ok(response);
+    }
 }
