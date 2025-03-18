421e0c837809e64d9d873b34bb932f48163397d5
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-18 12:10:27 -0300

    :sparkles: files required in createMovieRequest

--- src/Movies.Contracts/Requests/CreateMovieRequest.cs
+++ src/Movies.Contracts/Requests/CreateMovieRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace Movies.Contracts.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Contracts.Requests;
 
 public class CreateMovieRequest
 {
+	[Required(ErrorMessage = "The Title is required")]
 	public string Title { get; init; }
+	[Required(ErrorMessage = "The YearOfRelease is required")]
 	public int YearOfRelease { get; init; }
+	[MinLength(1, ErrorMessage = "The Genres is required")]
 	public IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
 }
