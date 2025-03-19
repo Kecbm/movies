c78bbc35b16d258ddd9b20236d30cd87cb5f44d0
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-19 12:49:54 -0300

    :sparkles: slug in movieResponse

--- src/Movies.Contracts/Responses/MovieResponse.cs
+++ src/Movies.Contracts/Responses/MovieResponse.cs
@@ -4,6 +4,7 @@ public class MovieResponse
 {
     public Guid Id { get; init; }
     public string Title { get; init; }
+    public string Slug { get; init; }
 	public int YearOfRelease { get; init; }
 	public IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
 }
