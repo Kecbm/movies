37c5942d750f7d77199a3311e6f7f0f51f49470b
Klecianny Melo <kleciannymelo@gmail.com>
2025-03-19 12:50:35 -0300

    :sparkles: generateSlug in movie model

--- src/Movies.Application/Models/Movie.cs
+++ src/Movies.Application/Models/Movie.cs
@@ -1,9 +1,20 @@
+using System.Text.RegularExpressions;
+
 namespace Movies.Application.Models;
 
 public class Movie
 {
     public Guid Id { get; init; }
     public string Title { get; init; }
+    public string Slug => GenerateSlug();
 	public int YearOfRelease { get; init; }
     public List<string> Genres { get; init; } = new();
+
+    private string GenerateSlug()
+    {
+        var sluggedTitle = Regex.Replace(Title, "[^0-9A-Za-z _-]", string.Empty)
+            .ToLower().Replace(" ", "-");
+
+        return $"{sluggedTitle}-{YearOfRelease}";
+    }
 }
